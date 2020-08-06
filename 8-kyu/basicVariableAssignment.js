81bfd939d9c25467fdfc7127fcf13889b7c4468a
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-08-06 09:52:10 -0400

    Added New Challeng

---
+++ 8-kyu/basicVariableAssignment.js
@@ -0,0 +1,11 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// var a == "code";
+// var b == "wa.rs";
+// name == a + b;
+
+//The error is because of th extra equal sign
+
+var a = "code";
+var b = "wa.rs";
+name = a + b;
