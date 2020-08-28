03ee1cf8b9d09fcfd9a9d27727e2b5606d686d0d
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-08-28 01:31:11 -0400

    Added New Challenge

---
+++ 7 kyu/beginnerSeries3.js
@@ -0,0 +1,15 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+function getSum(a, b) {
+  return ((a + b) * (Math.abs(a - b) + 1)) / 2;
+}
