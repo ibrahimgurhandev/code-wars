1ab48f7edd347907bce6d90e8bd1098c582d79d0
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-01 09:50:51 -0400

    Added New Challenge

---
+++ 8-kyu/messiGoal.js
@@ -0,0 +1,24 @@
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Define
+// Create three variables and store the appropriate values using the table above:
+
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals (javascript, C#, Java) or
+//  total_goals (python, ruby). totalGoals/total_goals should store the sum of
+//  all of Messi's goals for this year.
+
+let laLigaGoals = 43,
+  copaDelReyGoals = 5,
+  championsLeagueGoals = 10,
+  totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
