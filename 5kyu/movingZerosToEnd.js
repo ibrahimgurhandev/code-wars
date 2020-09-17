daf7fb3a3833575ad27e9a9843a3836912b20d30
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-17 14:04:26 -0400

    added New Challenge

---
+++ 5kyu/movingZerosToEnd.js
@@ -0,0 +1,8 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+    arr2 = arr.filter((item) => item === 0)
+    return arr.filter((item) => item !== 0).concat(arr2)
+}
