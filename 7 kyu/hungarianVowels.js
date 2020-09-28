dd3effbd9ee834e3c6987bf150d24dbb3b569504
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-28 11:55:11 -0400

    New challenge

---
+++ 7 kyu/hungarianVowels.js
@@ -0,0 +1,27 @@
+function dative(word) {
+
+    if(/[aáoóuú$]/.test(word.split(''))){
+      
+  return word + "nak"
+      
+    }
+    else{
+      
+    return word + "nek"
+    }
+  }
+
+//   Vowel harmony is a phenomenon in some languages. It means that "A vowel or vowels in a word are changed to sound the same (thus "in harmony.")" (wikipedia). This kata is based on vowel harmony in Hungarian.
+
+// Task:
+// Your goal is to create a function dative() (Dative() in C#) which returns the valid form of a valid Hungarian word w in dative case i. e. append the correct suffix nek or nak to the word w based on vowel harmony rules.
+
+// Vowel Harmony Rules (simplified)
+// When the last vowel in the word is
+
+// a front vowel (e, é, i, í, ö, ő, ü, ű) the suffix is -nek
+// a back vowel (a, á, o, ó, u, ú) the suffix is -nak
+// Examples:
+// dative("ablak") == "ablaknak"
+// dative("szék") == "széknek"
+// dative("otthon") == "otthonnak"
