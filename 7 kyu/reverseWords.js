6aff61643706edc18390cea99698158328785cc6
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-30 17:13:31 -0400

    todays challenge

---
+++ 7 kyu/reverseWords.js
@@ -0,0 +1,10 @@
+function reverseWords(str) {
+    return str.split(' ').map(item=> [...item].reverse().join('')).join(' ')
+    
+   }
+
+//    Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
