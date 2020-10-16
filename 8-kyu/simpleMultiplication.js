ae72987911156b9b2db646040dff0bb76b7e7a7d
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-10-15 23:41:16 -0400

    today's challenge

---
+++ 8-kyu/simpleMultiplication.js
@@ -0,0 +1,6 @@
+function simpleMultiplication(number) {
+    return number%2===0?number*8:number*9
+}
+
+// https://www.codewars.com/kata/583710ccaa6717322c000105
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
