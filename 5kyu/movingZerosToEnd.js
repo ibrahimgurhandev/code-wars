a61d1d0191a467c734a2a95992c715334f96deed
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-10-23 20:48:05 -0400

    new challenge

--- 5kyu/movingZerosToEnd.js
+++ 5kyu/movingZerosToEnd.js
@@ -3,6 +3,10 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 var moveZeros = function (arr) {
-    arr2 = arr.filter((item) => item === 0)
-    return arr.filter((item) => item !== 0).concat(arr2)
+   let arr2 = arr.filter((x) => x === 0)
+   let arr3 = arr.filter(x => x !== 0)
+   return arr3.concat(arr2)
+    //return arr.filter((item) => item !== 0).concat(arr2)
 }
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
