c71e98be7adfb9923ecf87adcc037fa93e5778c9
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-11-03 05:51:31 -0500

    new challenge

---
+++ 7 kyu/exesAndOhs.js
@@ -0,0 +1,15 @@
+XO = (str) => {
+    console.log(str)
+    return str.split('').filter(x=>x==='o'||x==='O').length===str.split('').filter(x=>x==='x'||x==='X').length}
+
+
+// https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
