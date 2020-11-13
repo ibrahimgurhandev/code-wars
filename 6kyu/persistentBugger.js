f0a1150ac47f4759e6fa37a870de3315a8a710b6
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-11-13 17:22:32 -0500

    today's challenge

---
+++ 6kyu/persistentBugger.js
@@ -0,0 +1,24 @@
+const persistence = (num) => {
+    var i = 0;
+    num = String(num)
+    while (num.length > 1) {
+      i++;
+      num = Array.from(num,Number).reduce((a,c) => a * c).toString()
+    }
+    return i
+ }
+
+
+
+//  https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+//  Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+
+//  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+//                        // and 4 has only one digit
+
+//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//                         // 1*2*6 = 12, and finally 1*2 = 2
+
+//  persistence(4) === 0 // because 4 is already a one-digit number
