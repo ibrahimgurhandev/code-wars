9e8398108f100b22f1c7ca1ae0807fcaeb77865c
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-11-17 07:43:11 -0500

    new challenge

---
+++ 8-kyu/convertToString.js
@@ -0,0 +1,14 @@
+function numberToString(num) {
+    return num.toString()
+  }
+
+// java solution
+
+// class Kata {
+//     public static String numberToString(int num) {
+//       return Integer.toString(num);
+//     }
+//   }
+
+
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
